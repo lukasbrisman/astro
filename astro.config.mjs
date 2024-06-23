8b605a517be373dfe183209400d5960aaf1a315f
lukasbrisman <l@brisman.se>
2024-06-23 20:16:04 +0200

    hejhej

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
-import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import tailwind from "@astrojs/tailwind"
-const env = loadEnv("", process.cwd(), 'STORYBLOK')
- 
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import tailwind from "@astrojs/tailwind";
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 export default defineConfig({
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
-    components: {
+    }),
+    tailwind()
+  ],
+  components: {
     page: 'storyblok/Page',
     feature: 'storyblok/Feature',
     grid: 'storyblok/Grid',
     teaser: 'storyblok/Teaser',
   },
-    }),
-    tailwind()
-  ],
-  },
-})
+});
+
